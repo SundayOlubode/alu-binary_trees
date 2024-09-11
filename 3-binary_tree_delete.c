160ee5a9d0d6e12dcb34fd320d2ab4051f3dc117
Sam <s.olubode@alustudent.com>
2024-09-11 14:28:24 +0100

    3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+binary_tree_t *binary_tree_delete_recursive(binary_tree_t *tree)
+{
+	binary_tree_t *left_node;
+	binary_tree_t *right_node;
+
+	if (tree == NULL)
+		return (NULL);
+
+	left_node = binary_tree_delete_recursive(tree->left);
+	if (left_node)
+	{
+		free(left_node);
+	}
+	right_node = binary_tree_delete_recursive(tree->right);
+	if (right_node)
+	{
+		free(right_node);
+	}
+	return (tree);
+}
+
 /**
  * binary_tree_delete - Delete a Binary tree
  *
@@ -10,9 +31,5 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree != NULL)
-	{
-		free(tree);
-	}
-	return;
+	binary_tree_delete_recursive(tree);
 }
