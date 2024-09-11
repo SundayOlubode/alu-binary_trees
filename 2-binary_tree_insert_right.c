dbb643593aedabc5221fed1be5e9eb36caf7ca3d
Sam <s.olubode@alustudent.com>
2024-09-11 13:49:20 +0100

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,31 +11,31 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-  binary_tree_t *new_right_child;
+	binary_tree_t *new_right_child;
 
-  if (parent == NULL)
-  {
-    return (NULL);
-  }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-  new_right_child = malloc(sizeof(binary_tree_t));
+	new_right_child = malloc(sizeof(binary_tree_t));
 
-  if (new_right_child == NULL)
-  {
-    return (NULL);
-  }
+	if (new_right_child == NULL)
+	{
+		return (NULL);
+	}
 
-  new_right_child->n = value;
-  new_right_child->right = NULL;
-  new_right_child->right = NULL;
-  new_right_child->parent = parent;
+	new_right_child->n = value;
+	new_right_child->right = NULL;
+	new_right_child->right = NULL;
+	new_right_child->parent = parent;
 
-  if (parent->right != NULL)
-  {
-    new_right_child->right = parent->right;
-    parent->right->parent = new_right_child;
-  }
-  parent->right = new_right_child;
+	if (parent->right != NULL)
+	{
+		new_right_child->right = parent->right;
+		parent->right->parent = new_right_child;
+	}
+	parent->right = new_right_child;
 
-  return (new_right_child);
+	return (new_right_child);
 }
