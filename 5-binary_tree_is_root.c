9b2f842779cff304e80a86eed1f6fb0897b32a56
Sam <s.olubode@alustudent.com>
2024-09-11 16:30:44 +0100

    5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,7 +3,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - Check if node is leaf
+ * binary_tree_is_root - Check if node is root
  *
  * @node: Pointer to (sub)tree
  * Return: 1 | 0
@@ -11,6 +11,7 @@
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	int res;
+
 	if (node == NULL)
 	{
 		return (0);
