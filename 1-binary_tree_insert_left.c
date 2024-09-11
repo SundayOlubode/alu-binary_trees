c2ef9c5acecd4e2142a7844be01835e0c6d00a3e
Sam <s.olubode@alustudent.com>
2024-09-11 13:10:33 +0100

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,36 +11,36 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_left_node;
-	binary_tree_t *old_left_node;
+	binary_tree_t *new_left_child;
+	binary_tree_t *old_left_child;
 
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
 
-	new_left_node = malloc(sizeof(binary_tree_t));
+	new_left_child = malloc(sizeof(binary_tree_t));
 
-	if (new_left_node == NULL)
+	if (new_left_child == NULL)
 	{
 		return (NULL);
 	}
 
-	new_left_node->n = value;
-	new_left_node->left = NULL;
-	new_left_node->right = NULL;
-	new_left_node->parent = parent;
+	new_left_child->n = value;
+	new_left_child->left = NULL;
+	new_left_child->right = NULL;
+	new_left_child->parent = parent;
 
-	if (parent->left)
+	if (parent->left == NULL)
 	{
-		old_left_node = parent->left;
-		parent->left = new_left_node;
-		new_left_node->left = old_left_node;
+		old_left_child = parent->left;
+		parent->left = new_left_child;
+		new_left_child->left = old_left_child;
 	}
 	else
 	{
-		parent->left = new_left_node;
+		parent->left = new_left_child;
 	}
 
-	return (new_left_node);
+	return (new_left_child);
 }
