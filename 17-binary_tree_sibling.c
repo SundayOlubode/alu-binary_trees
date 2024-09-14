9a14053733f19ae81e58c374049a4b43735e39c6
Sam <s.olubode@alustudent.com>
2024-09-14 19:52:20 +0100

    16-binary_tree_is_perfect.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,17 +9,17 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-  if (node == NULL)
-    return (NULL);
+	if (node == NULL)
+		return (NULL);
 
-  if (node->parent == NULL)
-    return (NULL);
+	if (node->parent == NULL)
+		return (NULL);
 
-  if (node->parent->left == node)
-    return (node->parent->right);
+	if (node->parent->left == node)
+		return (node->parent->right);
 
-  if (node->parent->right == node)
-    return (node->parent->left);
+	if (node->parent->right == node)
+		return (node->parent->left);
 
-  return (NULL);
+	return (NULL);
 }
