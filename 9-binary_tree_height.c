9dc7fc2a966f13418c6ca5da1114e461efd7c5fc
Sam <s.olubode@alustudent.com>
2024-09-14 16:36:12 +0100

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,17 +10,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  size_t left_height, right_height, H;
+	size_t left_height, right_height, H;
 
-  if (tree == NULL)
-  {
-    return (0);
-  }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-  left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
-  right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
 
-  H = left_height > right_height ? left_height : right_height;
+	H = left_height > right_height ? left_height : right_height;
 
-  return (H);
+	return (H);
 }
